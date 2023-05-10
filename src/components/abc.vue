46b444c3b56fb7e1621c763dbd639ceec6d2987d
shreyansh <shreyansh.redjinni@gmail.com>
2023-05-10 12:52:50 +0530

    date 08 to 09 try to set svg open and close in password field

---
+++ src/components/abc.vue
@@ -0,0 +1,76 @@
+<template>
+<!-- <div>
+  <input type="password" v-model="password">
+  <button @click="toggleVisibility">
+    {{ showPassword ? 'Hide' : 'Show' }} Password
+  </button>
+  <div v-if="showPassword">
+    {{ password }}
+  </div> 
+</div> -->
+
+<!-- <div>
+    <input type="password" v-model="password">
+    <button @click="toggleVisibility">
+        <font-awesome-icon :icon="showPassword ? 'eye-slash' : 'eye'" />
+    </button>
+    <div v-if="showPassword">
+        {{ password }}
+    </div>
+</div> -->
+
+<!-- <input type="password" v-model="password">
+<button @click="toggleVisibility">
+    <font-awesome-icon :icon="showPassword ? 'eye-slash' : 'eye'" />
+</button> -->
+<div>
+    
+   
+</div>
+</template>
+
+<script>
+export default {
+    name: "abc",
+    //     data() {
+    //         return {
+    //             password: '',
+    //             showPassword: false
+    //         };
+    //     },
+    //     methods: {
+    //         toggleVisibility() {
+    //             this.showPassword = !this.showPassword;
+    //         }
+    //     }
+    // setup() {
+    //     const password = ref('');
+    //     const showPassword = ref(false);
+
+    //     function toggleVisibility() {
+    //         showPassword.value = !showPassword.value;
+    //     }
+
+    //     return {
+    //         password,
+    //         showPassword,
+    //         toggleVisibility
+    //     };
+    // }
+    data() {
+        return {
+            password: '',
+            showPassword: false
+        };
+    },
+    methods: {
+        toggleVisibility() {
+            this.showPassword = !this.showPassword;
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
--- src/components/userlogin.vue
+++ src/components/userlogin.vue
@@ -8,8 +8,9 @@
                         <h2 class="welcome-heading">welcome back!</h2>
                         <p class="p-1">please enter credentials for sign in </p>
                         <input class="email" type="text" placeholder="email" id="email">
-                        <!-- <input class="password" type="password" placeholder="password" id="passwordField"><i class="fa fa-eye" aria-hidden="true" id="togglePassword" v-on:click="togglePassword1()"></i>  -->
+                        <!-- <input class="password" type="password" placeholder="password" id="passwordField"><i class="fa fa-eye-slash  fa-2x" aria-hidden="true" id="togglePassword" click="togglePassword1()"></i> -->
                         <input class="password" type="password" placeholder="password" id="password"> <img id="eye" src="../assets/Icon-eye.svg" alt="" v-on:click="togglePassword1()">
+                        <!-- <input class="password" type="password" placeholder="password" id="passfield">   -->
                         <div class=" col-lg-3 p-3">
                             <p>Remember me</p>
                         </div>
@@ -39,22 +40,21 @@ export default {
     name: 'userlogin',
 
     methods: {
-        // togglePassword1() {
-        //     var password= document.getElementById("password");
-        //     var togglePassword = document.getElementById("togglePassword");
-        //     if (password.type === "password") {
-        //         password.type = "text";
-        //         togglePassword.classList.remove("fa-eye-slash");
-        //         togglePassword.classList.add("fa-eye");
-        //     } else {
-        //         password.type = "password";
-        //         togglePassword.classList.remove("fa-eye");
-        //         togglePassword.classList.add("fa-eye-slash");
+        //     togglePassword1() {
+        //         var passwordField = document.getElementById("passwordField");
+        //         var togglePassword = document.getElementById("togglePassword");
+        //         if (passwordField.type === "password") {
+        //             passwordField.type = "text";
+        //             togglePassword.classList.remove("fa-eye-slash");
+        //             togglePassword.classList.add("fa-eye");
+        //         } else {
+        //             passwordField.type = "password";
+        //             togglePassword.classList.remove("fa-eye");
+        //             togglePassword.classList.add("fa-eye-slash");
 
+        //         }
         //     }
-
         // }
-
         togglePassword1() {
             var password = document.getElementById("password");
             var eye = document.getElementById("eye");
@@ -69,7 +69,6 @@ export default {
             }
 
         }
-
     }
 }
 </script>
@@ -111,7 +110,15 @@ export default {
     text-transform: capitalize;
 }
 
-#eye {
+#togglePassword {
+    margin-left: 72%;
+    transform: translateY(-50%);
+    margin-top: -4%;
+    cursor: pointer;
+    color: #BF9742;
+}
+
+#eye{
     margin-left: 72%;
     transform: translateY(-50%);
     margin-top: -4%;
@@ -119,7 +126,7 @@ export default {
 }
 
 .p-3 {
-    margin-top: -6%;
+    /* margin-top: -6%; */
     margin-left: -47%;
     font-family: 'Poppins', sans-serif;
     font-weight: 'regualar';
