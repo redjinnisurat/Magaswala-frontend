6433a05cef12a89b514f45a6036670c040058970
YA-redjinni <yash.ambaliya.redjinni@gmail.com>
2024-01-24 12:11:39 +0530

    Updated Code

--- src/axios.js
+++ src/axios.js
@@ -3,9 +3,9 @@ axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
 
 const instance = axios.create({
-  baseURL: "https://uat2-api.magaswala.com/public/api/",
+  // baseURL: "https://uat2-api.magaswala.com/public/api/",
 
-  // baseURL: "https://magaswalaapi.magaswala.com/public/api/",
+  baseURL: "https://magaswalaapi.magaswala.com/public/api/",
 
   headers: {
     "Content-Type": "application/json",
